4ad565fb0bc83771f97eb4d59064554ab6535e1c
Muhammad Saqib Ali <saqib.topdot@gmail.com>
2022-04-13 10:20:19 +0500

    docker updated

--- docker/start-container.sh
+++ docker/start-container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-useradd -u1000 sms_app
-adduser www-data sms_app
+useradd -u1000 deploy_app
+adduser www-data deploy_app
 chmod -R ug+rwx storage
 chmod -R ug+rwx bootstrap
+chown -R www-data:www-data storage
 composer install
 service supervisor start
 php-fpm
