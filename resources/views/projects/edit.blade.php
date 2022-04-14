6c112f919c7ee72323ab90344c6cd5f9bb6909a2
Muhammad Jawad <jawad.topdot@gmail.com>
2022-04-14 12:18:06 +0500

    Updated edit project form structure and added tabs.

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -15,11 +15,42 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <form action="{{ route('projects.update', $project) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-                        @include('projects.form')
-                    </form>
+                    <div class="row">
+                        <div class="col-md-2">
+                            <ul class="nav nav-tabs flex-md-column" id="myTab" role="tablist">
+                                <li class="nav-item" role="presentation">
+                                    <button class="btn active w-100" id="basic-info-tab" data-bs-toggle="tab"
+                                        data-bs-target="#basic-info" type="button" role="tab" aria-controls="basic-info"
+                                        aria-selected="true">Basic Info</button>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                    <button class="btn w-100" id="profile-tab" data-bs-toggle="tab"
+                                        data-bs-target="#profile" type="button" role="tab" aria-controls="profile"
+                                        aria-selected="false">Repository Info</button>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                    <button class="btn w-100" id="contact-tab" data-bs-toggle="tab"
+                                        data-bs-target="#contact" type="button" role="tab" aria-controls="contact"
+                                        aria-selected="false">Contact</button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-10">
+                            <div class="tab-content p-2" id="myTabContent">
+                                <div class="tab-pane fade show active" id="basic-info" role="tabpanel" aria-labelledby="basic-info-tab">
+                                    <form action="{{ route('projects.update', $project) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        @include('projects.form')
+                                    </form>
+                                </div>
+                                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">...
+                                </div>
+                                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">...
+                                </div>
+                            </div>
+                        </div>
+                    </div>                   
                 </div>
             </div>
         </div>
