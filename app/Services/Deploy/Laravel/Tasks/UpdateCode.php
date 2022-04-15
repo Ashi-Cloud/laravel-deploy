deecc4df2818a2f674d1a295828c97231487fb77
Muhammad Jawad <jawad.topdot@gmail.com>
2022-04-15 14:54:39 +0500

    Updated Host test method to resolve a bug Configure "UpdateCode" task to work with the provided ssh key file with project

--- app/Services/Deploy/Laravel/Tasks/UpdateCode.php
+++ app/Services/Deploy/Laravel/Tasks/UpdateCode.php
@@ -15,14 +15,22 @@ class UpdateCode extends Task
         $currentReleaseName = trim($this->host->runCommand("cat {$this->deployPath}/.dep/release.lock"),"\n");
         $currentReleasePath = "{$this->deployPath}/releases/{$currentReleaseName}";
 
+        $git_command = "git";
+        if(!empty($key = $this->project->git_ssh_key)){
+            $git_command = "GIT_SSH_COMMAND='ssh -i {$key} -o IdentitiesOnly=yes' git";
+        }
+
         /**
          * Check if already a git repo
          */
-        $this->host->runCommand("[ -f {$bare}/HEAD ] || git clone --mirror {$this->repository} $bare 2>&1");
-
-        $this->host->runCommand("cd $bare && git remote update");
-
-        $this->host->runCommand("cd $bare && git archive {$this->branch} | tar -x -f - -C {$currentReleasePath} 2>&1");
+        $this->host->runCommand("[ -f {$bare}/HEAD ] || {$git_command} clone --mirror {$this->repository} $bare 2>&1");
+        if(
+            !$this->host->test("[ -f {$bare}/HEAD ]")
+            || !$this->host->test("cd $bare && {$git_command} remote update")
+            || !$this->host->test("cd $bare && {$git_command} archive {$this->branch} | tar -x -f - -C {$currentReleasePath} 2>&1")
+        ){
+            throw new Exception("Repository not found. Please make sure you have the correct access rights and the repository exists.");
+        }
 
         $this->host->runCommand("echo {$currentReleaseName} > {$this->deployPath}/.dep/latest_release");
         
